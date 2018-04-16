5e857fa7023a33a46c1fc748254395fd6ac257a5
linmaorong <linmaorong@peiwo.cn>
2018-04-16 16:02:56 +0800

    Build a framework

---
+++ config/production_test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author:
+
+from default import *
+
+ENV_TEST = True
---
+++ core/database.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author:
+
+import config
+
+
+class PGBase(object):
+    pass
